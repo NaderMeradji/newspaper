031e2fa0b01694fbfd82276b082ef770e65ec606
Nader <nader.meradji@gmail.com>
2017-03-20 14:38:03 +0000

    AuthorStore

---
+++ storage/framework/views/1771ad226b01534251e7628b097aee7cb20499ef.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<title></title>
+
+</head>
+
+
+<body>
+<h1>
+<li>
+  Author
+<ul>
+<li><?php echo e($author->name); ?></li>
+<li><?php echo e($author->email); ?></li>
+<li><?php echo e($author->created_at); ?></li>
+</ul>
+</li>
+</h1>
+</body>
+</html>
---
+++ storage/framework/views/3a6db76f9346e30f7a7c28b634895c00b1d71fdb.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<title></title>
+
+</head>
+
+
+<body>
+<h1>
+<li>
+  Authors
+<ul>
+
+  <?php $__currentLoopData = $author; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $author): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+  <li><?php echo e($author->name); ?></li>
+  <li><?php echo e($author->email); ?></li>
+  <li><?php echo e($author->created_at); ?></li>
+
+  <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+</ul>
+</li>
+</h1>
+</body>
+</html>
